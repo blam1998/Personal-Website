92f986c802ad1647f14ae97db6a9e9fd67e0f7aa
blam1998 <57881360+blam1998@users.noreply.github.com>
2022-07-31 20:44:06 -0700

    Added personal website and changed CSS to make website look nicer

--- scripts/homeJquery.js
+++ scripts/homeJquery.js
@@ -1,19 +1,14 @@
 
 
-$("#form1").on("submit",function(e){
-    e.preventDefault();
-    let payload = {};
-    let elements = this.elements;
-    for (i = 0; i < elements.length; ++i){
-        if (elements[i].type !== "submit"){
-            payload[elements[i].name] = elements[i].value;
-        }
-    }
 
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST','/');
-    xhr.setRequestHeader('Content-Type','application/json');
-    console.log(payload);
-    let payloadString = JSON.stringify(payload);
-    xhr.send(payloadString);
+
+$('.linkCollapsible').on('click',function(e){
+    e.preventDefault();
+    var nextDiv = $(this).parent().next(".collapsibleContent");
+    if (nextDiv.is(':visible')){
+        nextDiv.hide();
+    }
+    else{
+        nextDiv.show();
+    }
 })
